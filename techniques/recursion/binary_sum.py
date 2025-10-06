ac7f781f5df60723208b8a372e358bf8fd2e0d4f
KhaledHussein1 <khaled.hussein.work@gmail.com>
2025-10-05 20:36:28 -0400

    recursion chapter complete

---
+++ techniques/recursion/binary_sum.py
@@ -0,0 +1,15 @@
+#
+def binary_sum(S, start, stop):
+    """Return the sum of the numbers in implicit slice S[start:stop]"""
+    if start>=stop: # zero elements in slice
+        return 0
+    elif start == stop-1: # one element in slice
+        return S[start]
+    else: # two or more elements in slice
+        mid = (start+ stop) // 2
+        return binary_sum(S, start, mid) + binary_sum(S, mid, stop)
+
+if __name__ == "__main__":
+    S = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    result = binary_sum(S, 0, 9)
+    print(result)
---
+++ techniques/recursion/fibonacci.py
@@ -0,0 +1,20 @@
+#O(n^2)
+def bad_fibonacci(n):
+    """Return the nth Fibonacci number."""
+    if n<=1:
+        return n
+    else:
+        return bad_fibonacci(n-2) + bad_fibonacci(n-1)
+
+#O(n)
+def good_fibonacci(n):
+    """Return pair of Fibonacci numbers, F(n) and F(n-1)"""
+    if n<=1:
+        return(n,0)
+    else: 
+        (a,b) = good_fibonacci(n-1)
+        return (a+b, a)
+
+if __name__ == "__main__":
+    print(bad_fibonacci(5))
+    print(good_fibonacci(5))
