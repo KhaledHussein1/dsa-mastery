771824df47dfb7669694c655d08b1f9c9537493b
KhaledHussein1 <khaled.hussein.work@gmail.com>
2025-10-13 13:18:24 -0400

    stack implementation

---
+++ data_structures/stacks_queues/stacks/stack.py
@@ -0,0 +1,56 @@
+class Empty(Exception):
+    """Error attempting to access an element from an empty container."""
+    pass
+
+class ArrayStack:
+    """LIFO Stack implementation using a Python list as underlying storage"""
+    
+    def __init__(self):
+        """Create an empty stack."""
+        self._data = [] # nonpublic list instance
+    
+    def __len__(self): # O(1)
+        """Return the number of elements in the stack"""
+        return len(self._data)
+    
+    def is_empty(self): # O(1)
+        """Return True if the stack is empty"""
+        return len(self._data) == 0
+    
+    def push(self, e): # O(1)
+        """Add element e to the top of the stack"""
+        self._data.append(e)
+    
+    def top(self): # O(1)
+        """Return (but not remove) element at the top of the stack
+        
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+    
+    def pop(self): # O(1)
+        """Remove and return the element from the top of the stack
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
+
+S = ArrayStack( )
+S.push(5)
+S.push(3)
+print(len(S))
+print(S.pop())
+print(S.is_empty())
+print(S.pop())
+print(S.is_empty())
+S.push(7)
+S.push(9)
+print(S.top())
+S.push(4)
+print(len(S))
+print(S.pop())
+S.push(6)
